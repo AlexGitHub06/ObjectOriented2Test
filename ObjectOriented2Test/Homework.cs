664546d03240713bd2371d2a56d2f2e33576efc1
Alexander Rathour <RathouA@StPaulsSchool.org.uk>
2023-01-15 23:33:13 +0000

    Add project files.

---
+++ ObjectOriented2Test/Homework.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOriented2Test
+{
+    internal class Homework
+    {
+        //Fields
+        private int _estimatedTimeToComplete;    
+
+        //Properties
+        public string Subject { get; private set; }
+        public int Difficulty { get; private set; }
+        public DateTime DueDate { get; private set; }  
+
+        //Constructor
+        public Homework(string subject, int difficulty, DateTime dueDate)
+        {
+            Subject = subject;
+            Difficulty = difficulty;
+            DueDate = dueDate;
+            UpdateEstimatedDif();
+        }
+
+        //Methods
+        public string GetUrgency()
+        {
+            if(DueDate < DateTime.Today.AddDays(_estimatedTimeToComplete))
+            {
+                return "high";
+            }
+            else if(DueDate == DateTime.Today.AddDays(_estimatedTimeToComplete))
+            {
+                return "medium";
+            }
+            else
+            {
+                return "low";
+            }
+        }
+
+        public string ChangeDifficulty(int difficulty) //this can also be done in set
+        {
+            if (difficulty <= 10 && difficulty > 0)
+            {
+                Difficulty = difficulty;
+                UpdateEstimatedDif();
+                return $"difficulty changed to {difficulty}";
+            }
+
+            else
+            {
+                return "invalid difficulty";
+            }
+
+            
+        }
+
+        private void UpdateEstimatedDif()
+        {
+            _estimatedTimeToComplete = Difficulty * 2;
+        }
+
+
+
+    }
+}
---
+++ ObjectOriented2Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ObjectOriented2Test
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Homework homework = new Homework("Computing", 5, DateTime.Today.AddDays(11));
+            Console.WriteLine(homework.Subject);
+            Console.WriteLine(homework.GetUrgency());
+            Console.WriteLine(homework.ChangeDifficulty(8));
+            Console.WriteLine($"Due date: {homework.DueDate}");
+            Console.WriteLine(homework.GetUrgency());
+            
+        }
+    }
+}
